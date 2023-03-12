c62844c0e8d7f92521a45107c4e44ed5664ae8d3
Whitley Horn <whitleyblaine@protonmail.com>
2023-03-12 13:41:28 -0400

    Finish ex 01 extra credit 02

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -5,19 +5,44 @@ import {Logo} from './components/logo'
 import {Dialog} from '@reach/dialog'
 import {VisuallyHidden} from '@reach/visually-hidden'
 
+const LoginForm = ({onSubmit, buttonText}) => {
+  const handleSubmit = e => {
+    e.preventDefault()
+    const {username, password} = e.target.elements
+
+    onSubmit({username: username.value, password: password.value})
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="username">Username:</label>
+      <input type="text" id="username" name="username" />
+      <label htmlFor="password">Password:</label>
+      <input type="text" id="password" name="password" />
+      <input type="submit" value={buttonText} />
+    </form>
+  )
+}
+
 const LoginDialog = ({isOpen, closeDialog}) => {
+  function handleSubmit(formData) {
+    console.log('login', formData)
+  }
   return (
     <Dialog isOpen={isOpen} onDismiss={closeDialog} aria-label="Login form">
       <button className="close-button" onClick={closeDialog}>
         <VisuallyHidden>Close</VisuallyHidden>
         <span aria-hidden>×</span>
       </button>
-      <h3>Login</h3>
+      <LoginForm onSubmit={handleSubmit} buttonText="Login" />
     </Dialog>
   )
 }
 
 const RegisterDialog = ({isOpen, closeDialog}) => {
+  function handleSubmit(formData) {
+    console.log('register', formData)
+  }
   return (
     <Dialog
       isOpen={isOpen}
@@ -29,11 +54,12 @@ const RegisterDialog = ({isOpen, closeDialog}) => {
         <span aria-hidden>×</span>
       </button>
       <h3>Register</h3>
+      <LoginForm onSubmit={handleSubmit} buttonText="Register" />
     </Dialog>
   )
 }
 
-const App = ({handleClick}) => {
+const App = () => {
   const [openDialog, setOpenDialog] = React.useState('none')
   const closeDialog = () => setOpenDialog('none')
 
@@ -58,4 +84,4 @@ const App = ({handleClick}) => {
 
 const rootElement = document.getElementById('root')
 const root = createRoot(rootElement)
-root.render(<App handleClick={name => alert(`${name} button clicked!`)} />)
+root.render(<App />)
