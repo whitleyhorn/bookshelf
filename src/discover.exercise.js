43db28a74a0c54110c2809ea9bfa9745535346c4
Whitley Horn <whitleyblaine@protonmail.com>
2023-03-16 12:19:17 -0400

    Finish ex 03

--- src/discover.exercise.js
+++ src/discover.exercise.js
@@ -5,32 +5,35 @@ import './bootstrap'
 import Tooltip from '@reach/tooltip'
 import {FaSearch} from 'react-icons/fa'
 import {Input, BookListUL, Spinner} from './components/lib'
+import {useState, useEffect} from 'react'
 import {BookRow} from './components/book-row'
-// 🐨 import the client from './utils/api-client'
+import {client} from './utils/api-client'
 
 function DiscoverBooksScreen() {
-  // 🐨 add state for status ('idle', 'loading', or 'success'), data, and query
-  const data = null // 💣 remove this, it's just here so the example doesn't explode
-  // 🐨 you'll also notice that we don't want to run the search until the
-  // user has submitted the form, so you'll need a boolean for that as well
-  // 💰 I called it "queried"
+  const [status, setStatus] = useState('idle') // 'idle', 'loading', 'success'
+  const [data, setData] = useState({})
+  const [query, setQuery] = useState('')
+  const [queried, setQueried] = useState(false)
 
-  // 🐨 Add a useEffect callback here for making the request with the
-  // client and updating the status and data.
-  // 💰 Here's the endpoint you'll call: `books?query=${encodeURIComponent(query)}`
-  // 🐨 remember, effect callbacks are called on the initial render too
-  // so you'll want to check if the user has submitted the form yet and if
-  // they haven't then return early (💰 this is what the queried state is for).
+  useEffect(() => {
+    if (!queried) return
+    const endpoint = `${
+      process.env.REACT_APP_API_URL
+    }/books?query=${encodeURIComponent(query)}`
+    client(endpoint, {}).then(result => {
+      setData(result)
+      setStatus('success')
+    })
+  }, [query, queried])
 
-  // 🐨 replace these with derived state values based on the status.
-  const isLoading = false
-  const isSuccess = false
+  const isLoading = status === 'loading'
+  const isSuccess = status === 'success'
 
   function handleSearchSubmit(event) {
-    // 🐨 call preventDefault on the event so you don't get a full page reload
-    // 🐨 set the queried state to true
-    // 🐨 set the query value which you can get from event.target.elements
-    // 💰 console.log(event.target.elements) if you're not sure.
+    event.preventDefault()
+    const value = event.target.elements.search.value
+    setQueried(true)
+    setQuery(value)
   }
 
   return (
