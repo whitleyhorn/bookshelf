e3f4d03dab64a272abace57374d15e10e8a7a2cf
Whitley Horn <whitleyblaine@protonmail.com>
2023-03-19 17:10:59 -0400

    Finish exercise 04 extra credit 04

--- src/utils/api-client.exercise.js
+++ src/utils/api-client.exercise.js
@@ -1,30 +1,35 @@
 import * as auth from 'auth-provider'
 const apiURL = process.env.REACT_APP_API_URL
 
-async function client(
+function client(
   endpoint,
-  {token, headers: customHeaders, ...customConfig} = {},
+  {token, data, headers: customHeaders, ...customConfig} = {},
 ) {
   const config = {
+    method: data ? 'POST' : 'GET',
     headers: {
       Authorization: token ? `Bearer ${token}` : undefined,
+      'Content-Type': data ? 'application/json' : undefined,
       ...customHeaders,
     },
+    body: data ? JSON.stringify(data) : undefined,
     ...customConfig,
   }
 
-  const response = await window.fetch(`${apiURL}/${endpoint}`, config)
-  if (response.status === 401) {
-    await auth.logout()
-    window.location.assign(window.location)
-    return Promise.reject({message: 'Please authenticate'})
-  }
-  const data = await response.json()
-  if (response.ok) {
-    return data
-  } else {
-    return Promise.reject(data)
-  }
+  return window.fetch(`${apiURL}/${endpoint}`, config).then(async response => {
+    if (response.status === 401) {
+      await auth.logout()
+      // refresh the page for them
+      window.location.assign(window.location)
+      return Promise.reject({message: 'Please re-authenticate.'})
+    }
+    const data = await response.json()
+    if (response.ok) {
+      return data
+    } else {
+      return Promise.reject(data)
+    }
+  })
 }
 
 export {client}
