392479150c19b522224b44c44b62eb18e14581bc
Whitley Horn <whitleyblaine@protonmail.com>
2023-03-23 15:44:36 -0400

    Use ternary operator to refactor

--- src/authenticated-app.exercise.js
+++ src/authenticated-app.exercise.js
@@ -56,33 +56,40 @@ function AuthenticatedApp({user, logout}) {
 
 function NavLink(props) {
   const match = useMatch(props.to)
-  const styles = [
-    {
-      display: 'block',
-      padding: '8px 15px 8px 10px',
-      margin: '5px 0',
-      width: '100%',
-      height: '100%',
-      color: colors.text,
-      borderRadius: '2px',
-      borderLeft: '5px solid transparent',
-      ':hover': {
-        color: colors.indigo,
-        textDecoration: 'none',
-        background: colors.gray10,
-      },
-    },
-  ]
-  if (match)
-    styles.push({
-      borderLeft: `5px solid ${colors.indigo}`,
-      background: colors.gray10,
-      ':hover': {
-        background: colors.gray20,
-      },
-    })
 
-  return <Link css={styles} {...props} />
+  return (
+    <Link
+      css={
+        ([
+          {
+            display: 'block',
+            padding: '8px 15px 8px 10px',
+            margin: '5px 0',
+            width: '100%',
+            height: '100%',
+            color: colors.text,
+            borderRadius: '2px',
+            borderLeft: '5px solid transparent',
+            ':hover': {
+              color: colors.indigo,
+              textDecoration: 'none',
+              background: colors.gray10,
+            },
+          },
+        ],
+        match
+          ? {
+              borderLeft: `5px solid ${colors.indigo}`,
+              background: colors.gray10,
+              ':hover': {
+                background: colors.gray20,
+              },
+            }
+          : null)
+      }
+      {...props}
+    />
+  )
 }
 
 function Nav() {
